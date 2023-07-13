d4c2a5a0b3ccc996b3672f3ceaebc3e736b23248
Teral <altero31@gmail.com>
2023-07-13 18:07:57 +0800

    Initial commit

---
+++ src/test/java/ter/bino/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ter.bino.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
