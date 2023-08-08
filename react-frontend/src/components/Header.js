db1a453bbdb70c03a69effa0e0b96fd2830ce4b9
!Ter Bino <altero31@gmail.com>
2023-08-08 18:16:03 +0800

    styling changes on page header

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -1,8 +1,8 @@
 
 
 function Header() {
-  return <div class="header flex flex-row justify-between items-center font-bold">
-    <h1 class="uppercase sm:text-md md:text-xl xl:text-3xl cursor-pointer">
+  return <div class="header flex flex-row justify-between items-center">
+    <h1 class="uppercase sm:text-md md:text-xl xl:text-3xl cursor-pointer font-bold">
         [
         <span class="hover:text-blue-700 mx-1">
             <span class="text-red-900 hover:text-violet-700">
@@ -13,8 +13,8 @@ function Header() {
         ]
     </h1>
     <div class="flex flex-row justify-end items-center uppercase sm:text-sm md:text-lg xl:text-2xl">
-        <span class="mr-2 nav-link">[ home ]</span>
-        <span class="mr-2 nav-link">[ create task ]</span>
+        [ <span class="m-2 nav-link text-white">home</span> ]
+        [ <span class="m-2 nav-link text-white">create task</span> ]
     </div>
   </div>;
 }
