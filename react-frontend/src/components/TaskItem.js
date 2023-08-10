fef98ec8f63c7740124e98f5726b5528f8ab1a05
!Ter Bino <altero31@gmail.com>
2023-08-10 14:25:26 +0800

    define proptypes on task item + delete prop

--- react-frontend/src/components/TaskItem.js
+++ react-frontend/src/components/TaskItem.js
@@ -1,6 +1,18 @@
+import PropTypes from 'prop-types';
 
+TaskItem.propTypes = {
+    task: PropTypes.shape({
+        taskId: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        deadline: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired,
+        important: PropTypes.bool.isRequired
+    }),
+    onDelete: PropTypes.func.isRequired
+};
 
-function TaskItem({task}) {
+function TaskItem({task, onDelete}) {
     return <div className={ "mb-4 shadow-lg rounded-2xl p-4 flex " + (false ? "bg-red-200":"bg-gray-100") }>
         <div className="flex-grow">
             <div className="font-bold truncate">
@@ -13,7 +25,7 @@ function TaskItem({task}) {
         </div>
         <div className="flex flex-col md:flex-row">
             <button className="px-2 my-auto md:mx-2 md:w-32 text-center bg-yellow-500 rounded-lg text-white">EDIT</button>
-            <button type="button" className="px-2 my-auto md:mx-2 md:w-32 text-center bg-red-500 rounded-lg text-white">DELETE</button>
+            <button type="button" onClick={()=>onDelete(task.taskId)} className="px-2 my-auto md:mx-2 md:w-32 text-center bg-red-500 rounded-lg text-white">DELETE</button>
         </div>
     </div>
 }
