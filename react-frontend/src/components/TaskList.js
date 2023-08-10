ecf4cdfb8471338501b4471951e27fc5adc5daf7
!Ter Bino <altero31@gmail.com>
2023-08-10 14:26:44 +0800

    fetchTask + deleteTask methods on TaskList.js

--- react-frontend/src/components/TaskList.js
+++ react-frontend/src/components/TaskList.js
@@ -1,38 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TaskItem from "./TaskItem";
+import axios from "axios";
 
 function TaskList() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: 'Task 1',
-            description: 'Description 1 cause idk',
-            deadline: "2023-10-31",
-            important: true,
-            completed: true
-        },
-        {
-            id: 2,
-            title: 'Task 2',
-            description: 'Description 2 cause idk',
-            deadline: "2023-08-31",
-            important: false,
-            completed: false
-        },
-        {
-            id: 3,
-            title: 'Task 3',
-            description: 'Description 3 cause idk',
-            deadline: "2024-08-31",
-            important: false,
-            completed: true
-        },
-    ]);
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+          const response = await axios.get("api/tasks/");
+          setTasks(response.data);
+        } catch (error) {
+          console.error(error);
+          alert("Unable to fetch tasks.");
+        }
+    };
+
+    const deleteItem = (id) => {
+        axios.delete(`api/tasks/delete/${id}`)
+            .then(response => {
+                alert(response.data);
+                fetchTasks();
+            })
+            .catch(error => {
+                alert(error)
+            }) 
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
 
     return <div>
         {tasks.length?
             tasks.map((task) => {
-                return <TaskItem key={task.id} task={task}/>
+                return <TaskItem key={task.taskId} task={task} onDelete={deleteItem}/>
             })
             :
             <div className="bg-gray-300 h-64 flex justify-center items-center rounded-2xl">
