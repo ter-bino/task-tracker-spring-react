75f065ddeb5f003b1f829e6ce116df164f9d86b3
!Ter Bino <altero31@gmail.com>
2023-08-12 10:20:03 +0800

    add navlink on website header logo

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -4,12 +4,12 @@ function Header() {
   return <div className="header flex flex-row justify-between items-center">
     <h1 className="uppercase sm:text-md md:text-xl xl:text-3xl cursor-pointer font-bold">
         [
-        <span className="hover:text-blue-700 mx-1">
+        <NavLink to="/" className="hover:text-blue-700 mx-1">
             <span className="text-red-900 hover:text-violet-700">
                 Task
             </span>
             Tracker!
-        </span> 
+        </NavLink> 
         ]
     </h1>
     <div className="flex flex-row justify-end items-center uppercase sm:text-sm md:text-lg xl:text-2xl">
