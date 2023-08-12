a652b2d66e22054ac24d82f4808992309bc30448
!Ter Bino <altero31@gmail.com>
2023-08-13 01:28:31 +0800

    working task list pagination

---
+++ react-frontend/src/components/Pagination.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+function Pagination({page, items, totalItems, totalPages, pageChanged}) {
+
+    const [pageNumbers, setPageNumbers] = useState([]);
+
+    Pagination.propTypes = {
+        page: PropTypes.number.isRequired,
+        items: PropTypes.number.isRequired,
+        totalItems: PropTypes.number.isRequired,
+        totalPages: PropTypes.number.isRequired,
+        pageChanged: PropTypes.func.isRequired,
+    };
+
+    useEffect(()=>{
+        let pages = [];
+        //Close to 1
+        if(page<4) {
+            for(let i=1; i<=(totalPages<3? totalPages: 3); i++)
+                pages.push(<button type="button" key={i} className={i===page? "btn pagination active": "btn pagination"} onClick={()=>pageChanged(i)}>{i}</button>);
+        //Close to last page
+        } else if (page>totalPages-3) {
+            for(let i=totalPages-2; i<=totalPages; i++)
+                pages.push(<button type="button" key={i} className={i===page? "btn pagination active": "btn pagination"} onClick={()=>pageChanged(i)}>{i}</button>);
+        }
+        //In the middle
+        else {
+            for(let i=page-2; i<=page+2; i++)
+                pages.push(<button type="button" key={i} className={i===page? "btn pagination active": "btn pagination"} onClick={()=>pageChanged(i)}>{i}</button>);
+        }
+        setPageNumbers(pages);
+    }, [page, items, totalItems, totalPages, pageChanged])
+
+    return <div>
+        <div className="flex justify-between items-center">
+            <div className="text-gray-500 text-sm">Showing {items} of {totalItems} tasks</div>
+            {totalPages!==1?
+                <div className="pagination text-sm">
+                    {page === 1? null: <button type="button" className="btn pagination" onClick={()=>pageChanged(1)}>First</button>}
+                    {page << 3? null: <button type="button" className="btn pagination" onClick={()=>pageChanged(page-1)}>Prev</button>}
+                    {pageNumbers}
+                    {page >> totalPages-2? null: <button type="button" className="btn pagination" onClick={()=>pageChanged(page+1)}>Next</button>}
+                    {page === totalPages? null: <button type="button" className="btn pagination" onClick={()=>pageChanged(totalPages)}>Last</button>}
+                </div>
+            :
+            <div className="text-gray-500 text-sm">No other pages</div>
+            }
+        </div>
+    </div>
+}
+
+export default Pagination;
--- react-frontend/src/pages/TaskList.js
+++ react-frontend/src/pages/TaskList.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import TaskItem from "./TaskItem";
 import axios from "axios";
+import Pagination from "../components/Pagination";
 
 function TaskList() {
     const [tasks, setTasks] = useState([]);
+    const [page, setPage] = useState(1);
+    const [perPage, setPerPage] = useState(5);
+    const [items, setItems] = useState(null);
+    const [totalItems, setTotalItems] = useState(null);
+    const [totalPages, setTotalPages] = useState(null);
 
     const fetchTasks = async () => {
         try {
-          const response = await axios.get("api/tasks/");
-          setTasks(response.data);
+          const response = await axios.get(`api/tasks/?page=${page-1}&perPage=${perPage}`);
+          setTasks(response.data.content);
+          setItems(response.data.numberOfElements)
+          setTotalItems(response.data.totalElements);
+          setTotalPages(response.data.totalPages);
         } catch (error) {
           console.error(error);
           alert("Unable to fetch tasks.");
@@ -28,9 +37,24 @@ function TaskList() {
 
     useEffect(() => {
         fetchTasks();
-    }, []);
+    }, [page, perPage]);
+
+    useEffect(() => {
+        setPage(1);
+        fetchTasks();
+    }, [perPage])
 
     return <div>
+        <div className="flex flex-row justify-start items-center mb-2">
+            Show
+            <select onChange={(e)=>setPerPage(e.target.value)} className="mx-2 focus:outline-none border-2 border-gray-400 rounded-lg">
+                <option value={5} defaultValue>5</option>
+                <option value={10}>10</option>
+                <option value={20}>20</option>
+                <option value={50}>50</option>
+            </select>
+            items
+        </div>
         {tasks.length?
             tasks.map((task) => {
                 return <TaskItem key={task.taskId} task={task} onDelete={deleteItem}/>
@@ -39,6 +63,10 @@ function TaskList() {
             <div className="bg-gray-300 h-64 flex justify-center items-center rounded-2xl">
                 <div className="text-center text-4xl text-gray-500 uppercase">no tasks to track</div>
             </div>
+        }
+        {tasks.length?
+            <Pagination page={page} items={items} totalItems={totalItems} totalPages={totalPages} pageChanged={setPage}/>
+            : null
         }
     </div>
 }
