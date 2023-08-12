28f2bf0a86f91d7d81c519b7e4420cecb7d71a0a
!Ter Bino <altero31@gmail.com>
2023-08-12 17:31:02 +0800

    fix taskitem code structure

--- react-frontend/src/components/TaskItem.js
+++ react-frontend/src/components/TaskItem.js
@@ -1,20 +1,24 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-
-TaskItem.propTypes = {
-    task: PropTypes.shape({
-        taskId: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        deadline: PropTypes.string.isRequired,
-        completed: PropTypes.bool.isRequired,
-        important: PropTypes.bool.isRequired
-    }),
-    onDelete: PropTypes.func.isRequired
-};
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function TaskItem({task, onDelete}) {
-    return <Link to={`/view/${task.taskId}`} className={ "mb-4 shadow-lg rounded-2xl p-4 flex " + (false ? "bg-red-200":"bg-gray-100") }>
+
+    const navigate = useNavigate();
+
+    TaskItem.propTypes = {
+        task: PropTypes.shape({
+            taskId: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            deadline: PropTypes.string.isRequired,
+            completed: PropTypes.bool.isRequired,
+            important: PropTypes.bool.isRequired
+        }),
+        onDelete: PropTypes.func.isRequired
+    };
+
+    return <div onClick={()=>navigate(`/view/${task.taskId}`)} className={ "mb-4 shadow-lg rounded-2xl p-4 flex " + (false ? "bg-red-200":"bg-gray-100") }>
         <div className="flex-grow w-3/5">
             <div className="font-bold truncate">
                 { task.title }
@@ -25,10 +29,10 @@ function TaskItem({task, onDelete}) {
             <p>{task.deadline}</p>
         </div>
         <div className="flex flex-col md:flex-row">
-            <Link to={`/edit/${task.taskId}`} className="btn warning my-auto md:mx-2 md:w-32 text-center">EDIT</Link>
-            <button type="button" onClick={()=>onDelete(task.taskId)} className="btn danger my-auto md:mx-2 md:w-32 text-center">DELETE</button>
+            <NavLink to={`/edit/${task.taskId}`} className="btn warning my-auto md:mx-2 md:w-32 text-center">EDIT</NavLink>
+            <button type="button" onClick={(e)=>{e.stopPropagation();onDelete(task.taskId)}} className="btn danger my-auto md:mx-2 md:w-32 text-center">DELETE</button>
         </div>
-    </Link>
+    </div>
 }
 
 export default TaskItem;
